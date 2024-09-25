cf3444b9d0a9dc5410fa7f0b2d4fe7e5578907a0
Hyper7711 <143863603+Hyper7711@users.noreply.github.com>
2024-09-25 15:29:36 +0530

    Adv_1
    Adding first feature of hint.In which user will get the Hint whether the guessed number is even or odd..

--- GTN_game.py
+++ GTN_game.py
@@ -6,6 +6,7 @@ attempts = 0
 while True:
     guess = int(input("Guess a number between 1 and 100: "))
     attempts += 1
+    
     if guess < number:
         print("Too low!")
     elif guess > number:
@@ -13,4 +14,10 @@ while True:
     else:
         print(f"Congratulations! You've guessed the number in {attempts} attempts.")
         break
-s
+    
+    # Provide a hint after 5 wrong guesses
+    if attempts == 5:
+        if number % 2 == 0:
+            print("Hint: The number is even.")
+        else:
+            print("Hint: The number is odd.")
